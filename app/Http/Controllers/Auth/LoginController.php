b79b474d4ba863b32b979906862e51cc6cbc3e43
EijeiM <78541681+EijeiM@users.noreply.github.com>
2021-07-16 00:53:59 +0800

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->user();
+
+        // Find User
+        $authUser = User::where('email', $user->email)->first();
+        if($authUser){
+            Auth::login($authUser);
+            return redirect()->route('home');
+        }
+        else{
+            $newUser = new User();
+            $newUser->email = $user->email;
+            $newUser->name = $user->name;
+            $newUser->email_verified_at = Date::now();
+            $newUser->userid = $user->id;
+            $newUser->avatar = $user->avatar;
+            $newUser->save();
+
+            // Login
+            Auth::login($newUser);
+            return redirect()->route('home');
+        }
+        
+    }
+
+}

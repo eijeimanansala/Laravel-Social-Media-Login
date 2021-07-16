dfc58b26dbf6f173e86c68b5afd43f532604397c
EijeiM <78541681+EijeiM@users.noreply.github.com>
2021-07-16 23:59:34 +0800

    Change tables in animals and cars

--- resources/views/animals.blade.php
+++ resources/views/animals.blade.php
@@ -4,14 +4,12 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">ID</th>
                 <th scope="col">Name</th>
                 <th scope="col">Types</th>
             </tr>
             <tbody>
                 @foreach ($animals as $animal)
                     <tr>
-                        <th scope="row">{{ $animal->id }}</th>
                         <td>{{ $animal->name }}</td>
                         <td>{{ $animal->types }}</td>
                     </tr>
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -4,14 +4,12 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">ID</th>
                 <th scope="col">Brand</th>
                 <th scope="col">Types</th>
             </tr>
             <tbody>
                 @foreach ($cars as $car)
                     <tr>
-                        <th scope="row">{{ $car->id }}</th>
                         <td>{{ $car->brand }}</td>
                         <td>{{ $car->types }}</td>
                     </tr>
